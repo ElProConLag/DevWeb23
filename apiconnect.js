eaf0ac516c8e9e116209e309ac0af16b208ee553
Camilo Pinto <camilo.pinto1@mail.udp.cl>
2023-07-15 15:36:22 -0400

    retirar and script code added

--- apiconnect.js
+++ apiconnect.js
@@ -1,50 +1,43 @@
-// Function to set a session cookie with an authentication token
+
 function setSessionToken(token) {
-    const cookieValue = `authToken=${token}; path=/;`;
-    document.cookie = cookieValue;
-  }
+  const date = new Date();
+  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000)); // set expiration date to one year from now
+  const expires = `expires=${date.toUTCString()}`;
+  const path = 'path=/';
+  const sameSite = 'SameSite=None';
+  const secure = 'Secure';
+  const cookieValue = `authToken=${token}; ${expires}; ${path}; ${sameSite}; ${secure}`;
+  document.cookie = cookieValue;
+}
+
+function deleteSessionToken() {
+  const date = new Date();
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000)); // set expiration date to one day ago
+  const expires = `expires=${date.toUTCString()}`;
+  const path = 'path=/';
+  const cookieValue = `authToken=; ${expires}; ${path}`;
+  document.cookie = cookieValue;
+}
   
-  // Function to delete the session cookie with the authentication token
-  function deleteSessionToken() {
-    const cookieValue = `authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    document.cookie = cookieValue;
-  }
-  
-  // Function to query movement information using the authentication token
-  async function getMovementInfo() {
-    const authToken = getAuthToken();
-    if (!authToken) {
-      throw new Error('Authentication token not found');
-    }
-  
-    const response = await fetch('http://54.156.208.82/movimientos', {
-      headers: {
-        Authorization: `Bearer ${authToken}`
-      }
-    });
-  
-    if (!response.ok) {
-      throw new Error('Failed to fetch movement information');
-    }
-  
-    const data = await response.json();
-    return data;
-  }
   
   // Function to check if the session cookie with the authentication token exists
   function checkSessionToken() {
     const authToken = getAuthToken();
+    // If the authentication token does not exist, return false
     if (!authToken) {
-      window.location.href = './ingreso.html';
+      return false;
     }
+    // If the authentication token exists, return true
+    return true;
   }
   
-  // Helper function to retrieve the authentication token from the session cookie
   function getAuthToken() {
-    const authTokenCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('authToken='));
-    if (authTokenCookie) {
-      return authTokenCookie.split('=')[1];
-    } else {
-      return null;
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith('authToken=')) {
+      return cookie.substring('authToken='.length, cookie.length);
     }
   }
+  return null;
+}
